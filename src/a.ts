bc3b9519902ba6dd2022171650be161e4a6d4355
xushankun <lovexsk@qq.com>
2024-07-31 16:16:30 +0800

    测测测

--- src/a.ts
+++ src/a.ts
@@ -1,6 +1,6 @@
 export const a = 'a'
 
 console.log('====================================')
-console.log(333)
+console.log(4444)
 console.log('====================================')
 export const b = 'b'
