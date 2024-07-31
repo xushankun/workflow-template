f76557ef2d57f0ecc729ce9385c20c9a090f1948
xushankun <lovexsk@qq.com>
2024-07-31 16:15:06 +0800

    feat

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,25 @@ module.exports = {
   extends: ['cz'],
   rules: {
     // 自定义规则
+    'type-enum': [
+      // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
+      2, // 当前验证的错误级别，2-表示错误级别的错误
+      'always',
+      [
+        // 泛型内容
+        'feat', // 新功能 feature
+        'fix', // 修复 bug
+        'docs', // 文档注释
+        'style', // 代码格式(不影响代码运行的变动)
+        'refactor', // 重构(既不增加新功能，也不是修复bug)
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build' // 打包
+      ]
+    ],
+    'type-case': [0, 'lowerCase'],
+    'type-empty': [2, 'never']
   }
 }
--- src/a.ts
+++ src/a.ts
@@ -1,6 +1,6 @@
 export const a = 'a'
 
 console.log('====================================')
-console.log(222)
+console.log(333)
 console.log('====================================')
 export const b = 'b'
